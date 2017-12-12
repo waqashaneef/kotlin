b6279b9eb5b11af2be79015ade77d64bcec45949
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-12-12 14:18:42 +0100

    Introduce Experimental and UseExperimental annotations

---
+++ compiler/testData/diagnostics/testsWithStdLib/experimental/typealias.kt
@@ -0,0 +1,12 @@
+// FILE: api.kt
+
+package api
+
+@Experimental
+@Target(AnnotationTarget.CLASS)
+annotation class ExperimentalAPI
+
+@ExperimentalAPI
+class Foo
+
+typealias Bar = <!EXPERIMENTAL_API_USAGE_ERROR!>Foo<!>
