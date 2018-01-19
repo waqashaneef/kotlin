2a919c721da77f380d113e24fbb9d8ba2579578e
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2018-01-19 03:40:25 +0300

    Fix android plugin dependencies for 171

--- plugins/android-extensions/android-extensions-idea/build.gradle.kts
+++ plugins/android-extensions/android-extensions-idea/build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     compile(project(":idea:idea-gradle"))
     compile(project(":plugins:android-extensions-compiler"))
     compileOnly(project(":kotlin-android-extensions-runtime"))
-    compileOnly(intellijPluginDep("android")) { includeJars("android.jar", "android-common.jar", "sdk-common.jar", "sdk-tools.jar") }
+    compileOnly(intellijPluginDep("android")) { includeJars("android", "android-common", "common", "sdk-common", "sdk-tools") }
     compileOnly(intellijPluginDep("Groovy")) { includeJars("Groovy.jar") }
     compileOnly(intellijDep()) { includeJars("extensions.jar", "openapi.jar", "util.jar", "idea.jar", "android-base-common.jar", rootProject = rootProject) }
 
--- plugins/lint/build.gradle.kts
+++ plugins/lint/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     compileOnly(intellijCoreDep()) { includeJars("intellij-core") }
     compileOnly(intellijDep()) { includeJars("util", "guava-19.0", "openapi", "idea", "asm-all") }
     compileOnly(intellijPluginDep("android")) {
-        includeJars("android", "android-common", "sdklib", "sdk-common", "sdk-tools",
+        includeJars("android", "android-common", "common", "sdklib", "sdk-common", "sdk-tools",
                     "repository", "lombok-ast-0.2.3", "layoutlib-api", "kxml2-2.3.0")
     }
 }
