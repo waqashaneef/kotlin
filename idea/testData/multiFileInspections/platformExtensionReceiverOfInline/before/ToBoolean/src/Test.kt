273eb84501d833ae292a928ece8a65b49a1a1947
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2018-01-22 13:51:53 +0300

    Add regex to inspection for nullable extension receiver of inline
    Related to KT-22303

--- idea/testData/multiFileInspections/platformExtensionReceiverOfInline/before/ToBoolean/src/Test.kt
+++ idea/testData/multiFileInspections/platformExtensionReceiverOfInline/before/ToBoolean/src/Test.kt
@@ -1,14 +1,15 @@
 fun test() {
     JavaClass.getNull().toBoolean() // ISE in runtime
+    JavaClass.getNull().toInt()     // OK
     "123".toBoolean()               // OK
     null.toBoolean()                // Compilation error, no inspection message
 
-    JavaClass.getNull().notInline() // OK
-    JavaClass.getMy().nonExtensionInlineFun() // OK
+    JavaClass.getNull().contentNotInline() // OK
+    JavaClass.getMy().contentNonExtensionInlineFun() // OK
 }
 
-fun String.notInline() {}
+fun String.contentNotInline() {}
 
 class My {
-    inline fun nonExtensionInlineFun() {}
+    inline fun contentNonExtensionInlineFun() {}
 }
